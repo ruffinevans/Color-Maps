54dd5b65337b95b75b5f82d7bda483b96245cef9
Ruffin Evans <ruffin@ruffinevans.com>
2015-04-04 19:36:06 -0400

    Fixed linear map, added function for map rescaling Fixed previous problem with linear color map. Also added a function to rescale the color map for data over an arbitrary range. Functionality should now be quite good.

--- ColorMaps.m
+++ ColorMaps.m
@@ -4,14 +4,14 @@
 (*This notebook defines color functions that are perceptually linear. For more discussion, see:*)
 (*http://mathematica.stackexchange.com/questions/64513/is-there-an-easy-way-to-use-matteo-niccolis-perceptual-color-maps-for-2d-plots*)
 (*https://mycarta.wordpress.com/2012/05/29/the-rainbow-is-dead-long-live-the-rainbow-series-outline/*)
-(*None of this work is mine -- I have just compiled it from the two above sources.*)
+(*None of this work is mine -- I have just compiled it from the two above sources and added a few functions to make it easy to use in Mathematica. *)
 
 
 (* ::Text:: *)
 (*Use current version of colormaps:*)
 
 
-webdir="https://raw.githubusercontent.com/ruffinevans/Color-Maps/master/Colormaps/"
+webdir="https://raw.githubusercontent.com/ruffinevans/Color-Maps/master/Colormaps/";
 
 
 (* ::Text:: *)
@@ -24,7 +24,7 @@ Cube1CM=Module[{colorlist},colorlist=Get[webdir<>"cube1_0-1.txt"];
 Evaluate[Blend[RGBColor@@@colorlist,#]&]];
 CubeYFCM=Module[{colorlist},colorlist=Get[webdir<>"cubeyf_0-1.txt"];
 Evaluate[Blend[RGBColor@@@colorlist,#]&]];
-LinearLCM=Module[{colorlist},colorlist=Get[webdir<>"inear_l_0-1.txt"];
+LinearLCM=Module[{colorlist},colorlist=Get[webdir<>"linear_l_0-1.txt"];
 Evaluate[Blend[RGBColor@@@colorlist,#]&]];
 JetCM=Module[{colorlist},colorlist=Get[webdir<>"jet.txt"];
 Evaluate[Blend[RGBColor@@@colorlist,#]&]];
@@ -38,12 +38,22 @@ ColorMapsExample[]:=TableForm[{{"JetCM (bad)","ParulaCM","Cube1CM","CubeYFCM","L
 
 
 (* ::Text:: *)
+(*Rescales color maps for convenience*)
+
+
+ScaleCF[CF_,data_]:=Module[{mi=Min@data,ma=Max@data},CF[(#-mi)/ma]&]
+ScaleCF::usage="ScaleCF[CF,data] which generates a new color function based on the min and max of the (1D) data. Alternatively a range {min, max} can be explicitly passed instead of data.";
+
+
+(* ::Text:: *)
 (*Readme*)
 
 
 Print["\!\(\*
 StyleBox[\"ColorMaps\",\nFontVariations->{\"Underline\"->True}]\)\!\(\*
-StyleBox[\" \",\nFontVariations->{\"Underline\"->True}]\)v1
+StyleBox[\" \",\nFontVariations->{\"Underline\"->True}]\)
 This package defines four new perceptually linear color functions for use in mathematica: ParulaCM, Cube1CM, CubeYFCM, and LinearLCM.
 I think that Cube1CM and LinearLCM are designed to work on positive and negative data. CubeYFCM is designed to work on strictly positive data.
+All data is expected to be scaled from 0 to 1 before plotting. Instead of rescaling the data, it is useful to rescale the color function istead based on the extrema of the data. This can be done with the function ScaleCF[CF,data] which generates a new color function based on the min and max of the (1D) data. Alternatively a range {min, max} can be explicitly passed instead of data.
+You should also pass ColorFunctionScaling->False as a flag to prevent Mathematica from \"helpfully\" rescaling the data.
 Run function ColorMapsExample[] for an example."];
