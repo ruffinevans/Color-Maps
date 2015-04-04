f1b6ab32e981c77e52ae16dfa672647d4f461a22
Ruffin Evans <ruffin@ruffinevans.com>
2015-04-03 23:47:06 -0400

    Made more package-like LinearLCM does not seem to work. Need to debug!

--- ColorMaps.m
+++ ColorMaps.m
@@ -7,31 +7,43 @@
 (*None of this work is mine -- I have just compiled it from the two above sources.*)
 
 
-(* ::Input:: *)
-(*SetDirectory[NotebookDirectory[]]*)
+(* ::Text:: *)
+(*Use current version of colormaps:*)
 
 
-(* ::Input:: *)
-(*ParulaCM=Module[{colorlist},colorlist=Get["Colormaps/parula.txt"];*)
-(*Evaluate[Blend[RGBColor@@@colorlist,#]&]];*)
-(*Cube1CM=Module[{colorlist},colorlist=Get["Colormaps/cube1_0-1.txt"];*)
-(*Evaluate[Blend[RGBColor@@@colorlist,#]&]];*)
-(*CubeYFCM=Module[{colorlist},colorlist=Get["Colormaps/cubeyf_0-1.txt"];*)
-(*Evaluate[Blend[RGBColor@@@colorlist,#]&]];*)
-(*LinearLCM=Module[{colorlist},colorlist=Get["Colormaps/linear_l_0-1.txt"];*)
-(*Evaluate[Blend[RGBColor@@@colorlist,#]&]];*)
-(*JetCM=Module[{colorlist},colorlist=Get["Colormaps/jet.txt"];*)
-(*Evaluate[Blend[RGBColor@@@colorlist,#]&]];*)
+webdir="https://raw.githubusercontent.com/ruffinevans/Color-Maps/master/Colormaps/"
 
 
-(* ::Input:: *)
-(*ColorMapsExample[]:=TableForm[{{"JetCM (bad)","ParulaCM","Cube1CM","CubeYFCM","LinearLCM"},BarLegend[{#,{0,1}}]&/@{JetCM,ParulaCM,Cube1CM,CubeYFCM,LinearLCM},DensityPlot[Cos[x] Sin[y],{x,-10,10},{y,-10,10},PlotRange->All,ColorFunction->#,PlotPoints->75]&/@{JetCM,ParulaCM,Cube1CM,CubeYFCM,LinearLCM}}]*)
+(* ::Text:: *)
+(*Code defining color functions*)
 
 
-(* ::Input:: *)
-(*Print["\!\(\**)
-(*StyleBox[\"ColorMaps\",\nFontVariations->{\"Underline\"->True}]\)\!\(\**)
-(*StyleBox[\" \",\nFontVariations->{\"Underline\"->True}]\)v1*)
-(*This package defines four new perceptually linear color functions for use in mathematica: ParulaCM, Cube1CM, CubeYFCM, and LinearLCM.*)
-(*I think that Cube1CM and LinearLCM are designed to work on positive and negative data. CubeYFCM is designed to work on strictly positive data.*)
-(*Run function ColorMapsExample[] for an example."];*)
+ParulaCM=Module[{colorlist},colorlist=Get[webdir<>"parula.txt"];
+Evaluate[Blend[RGBColor@@@colorlist,#]&]];
+Cube1CM=Module[{colorlist},colorlist=Get[webdir<>"cube1_0-1.txt"];
+Evaluate[Blend[RGBColor@@@colorlist,#]&]];
+CubeYFCM=Module[{colorlist},colorlist=Get[webdir<>"cubeyf_0-1.txt"];
+Evaluate[Blend[RGBColor@@@colorlist,#]&]];
+LinearLCM=Module[{colorlist},colorlist=Get[webdir<>"inear_l_0-1.txt"];
+Evaluate[Blend[RGBColor@@@colorlist,#]&]];
+JetCM=Module[{colorlist},colorlist=Get[webdir<>"jet.txt"];
+Evaluate[Blend[RGBColor@@@colorlist,#]&]];
+
+
+(* ::Text:: *)
+(*Generates exmple file*)
+
+
+ColorMapsExample[]:=TableForm[{{"JetCM (bad)","ParulaCM","Cube1CM","CubeYFCM","LinearLCM"},BarLegend[{#,{0,1}}]&/@{JetCM,ParulaCM,Cube1CM,CubeYFCM,LinearLCM},DensityPlot[Cos[x] Sin[y],{x,-10,10},{y,-10,10},PlotRange->All,ColorFunction->#,PlotPoints->75]&/@{JetCM,ParulaCM,Cube1CM,CubeYFCM,LinearLCM}}]
+
+
+(* ::Text:: *)
+(*Readme*)
+
+
+Print["\!\(\*
+StyleBox[\"ColorMaps\",\nFontVariations->{\"Underline\"->True}]\)\!\(\*
+StyleBox[\" \",\nFontVariations->{\"Underline\"->True}]\)v1
+This package defines four new perceptually linear color functions for use in mathematica: ParulaCM, Cube1CM, CubeYFCM, and LinearLCM.
+I think that Cube1CM and LinearLCM are designed to work on positive and negative data. CubeYFCM is designed to work on strictly positive data.
+Run function ColorMapsExample[] for an example."];
